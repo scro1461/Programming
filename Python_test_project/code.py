6e60f1047f6fc68ef957aba63874553c038434c7
scro1461 <skoczylis@me.com>
2018-09-19 23:01:19 +0100

    19/09/2018 23:00

--- Python_test_project/code.py
+++ Python_test_project/code.py
@@ -1,18 +1,10 @@
 from tinydb import TinyDB, Query
-from bottle import route, run
+from bottle import route, run, template
 
-to_do_listDB = TinyDB('to_do.json')
+@route('/test')
+def test(name='It works'):
+    return template('index', name = name)
 
-to_do_listDB.insert({'Name':'Jamie'})
-to_do_listDB.insert({'Name':'Aimee'})
-
-def printDB(a):
-    for item in a:
-        print(item)
-
-@route('/todo')
-def todo():
-    return str(printDB(to_do_listDB))
 run()
 
 
